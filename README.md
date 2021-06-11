0290295e6842b5d61a2f6b9ec892ef233d36e020
kshired <shiroed1211@naver.com>
2021-06-12 04:42:13 +0900

    add new feature

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SGCC-discord-bot
 
 SGCC Discord bot for checking entrance and exit at the '모각코' channel
+
+- [x] send message with user's name
--- index.js
+++ index.js
@@ -2,10 +2,29 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// init count
+// init values
 let channelCount = -1;
+let channelPeople = [];
+const messageOut = ['어디가ㅏㅏㅏㅏㅏ', '가지마ㅏㅏㅏㅏ'];
+
 // login discord bot
 client.login(process.env.TOKEN);
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.ceil(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+// compare comp with target
+const checkDiff = (origin, target) => {
+  for (let person of comp) {
+    if (!target.includes(person)) {
+      return person;
+    }
+  }
+  return '';
+};
 
 /* check how many people in the specific channel
    and send the message when count changes. */
@@ -13,21 +32,33 @@ const count = async () => {
   const channel = await client.channels.cache.find(
     (channel) => channel.id === process.env.COUNT_CHANNEL
   );
+
+  const tmp = Array.from(channel.members);
+  let curPeople = [];
+
+  for (let i = 0; i < tmp.length; ++i) {
+    curPeople.push(`${tmp[i][1].nickname}`);
+  }
+
   const curCount = channel.members.size;
   if (channelCount === -1) {
     channelCount = curCount;
+    channelPeople = curPeople;
     return;
   }
   if (channelCount > curCount) {
+    const message = checkDiff(channelPeople, tmpPeople);
     await client.channels.cache
       .find((channel) => channel.id === process.env.TARGET_CHANNEL)
-      .send('어디가ㅏㅏㅏ');
+      .send(`${message} ${messageOut[getRandomInt(0, 2)]}`);
   } else if (channelCount < curCount) {
+    const message = checkDiff(tmpPeople, channelPeople);
     await client.channels.cache
       .find((channel) => channel.id === process.env.TARGET_CHANNEL)
-      .send('왔구나');
+      .send(`${message} 왔구나`);
   }
   channelCount = curCount;
+  channelPeople = curPeople;
 };
 
 /* Recursive setTimeout guarantees the given delay 
@@ -37,7 +68,7 @@ const mySetInterval = () => {
   setTimeout(async () => {
     await count();
     mySetInterval();
-  }, 1500);
+  }, 1000);
 };
 
 mySetInterval();
